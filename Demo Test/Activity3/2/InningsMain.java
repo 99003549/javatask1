cdce8cbb1abe542d02b69acb3c6e893ebaaf1593
99003549 <78538040+99003549@users.noreply.github.com>
2021-03-03 14:00:38 +0530

    Update and rename Demo Test/src/Activity3/InningsMain.java to Demo Test/Activity3/2/InningsMain.java

--- Demo Test/Activity3/2/InningsMain.java
+++ Demo Test/Activity3/2/InningsMain.java
@@ -4,19 +4,22 @@ import java.util.Scanner;
 
 public class InningsMain {
 	public static void main(String[] args) {
+		String name;
+		String session;
+		int runs;
 		Innings i=new Innings();
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the teamname:");
-		String name=s.next();
+		name=s.next();
 		System.out.println("Enter session:");
-		String session=s.next();
+		session=s.next();
 		System.out.println("Enter runs:");
-		int runs=s.nextInt();
+		runs=s.nextInt();
 		
-		i.setTeamname(name);
-	    i.setInningsname(session);
+		i.setInningsname(session);
+	    i.setTeamname(name);
 		i.setRuns(runs);
-		System.out.println("Enter the Team Name: "+i.getTeamname()+"\nEnter Address: "+i.getInningsname()+"\nEnter Mobile: "+i.getRuns());
+	    Innings.displayInningsDetails();
 	}
 }
 
